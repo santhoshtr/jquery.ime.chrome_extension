faf7ebcb976444aabf753f9321e4433aa20cc94b
Santhosh Thottingal <santhosh.thottingal@gmail.com>
2012-12-09 20:36:29 +0530

    Integrate jquery.uls based language selector and persist the input method and language preferences

--- update.sh
+++ update.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
 sed -i -e "s/\.\.\/images/chrome-extension:\/\/__MSG_\@\@extension_id__\/libs\/jquery.ime\/images/g" libs/jquery.ime/css/jquery.ime.css
+sed -i -e "s/\.\.\/images/chrome-extension:\/\/__MSG_\@\@extension_id__\/libs\/jquery.uls\/images/g" libs/jquery.uls/css/jquery.uls.css
